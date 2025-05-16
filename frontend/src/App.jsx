898671f291668996996ac9a8c4bd81d5881d47d9
JasonWThinkpad <jasonwlenovo@DESKTOP-OC5BROQ.>
2025-05-16 19:48:15 +1000

    initial setup: supabase material ui, router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
