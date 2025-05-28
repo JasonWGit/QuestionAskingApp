d5d52e6c8806ac4badab4758b0a62ffabb55fd9e
JasonWThinkpad <jasonwlenovo@DESKTOP-OC5BROQ.>
2025-05-28 17:37:24 +1000

    added Todo comments

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -50,6 +50,7 @@ export default function LoginPage() {
     }
     console.log(data);
     showSuccessAlert('Successfully logged in');
+    // probably fetch the users role here from the profiles table and conditionally navigate them to either the normal dashboard or the admin dashboard
     navigate('/dashboard');
   }
 
