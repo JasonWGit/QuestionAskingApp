e7e5dc805f13884493b77857e0cdb26f88541325
JasonW <jason.wang911666@gmail.com>
2025-06-04 17:41:39 +1000

    added commenting functionality to question pages

--- frontend/src/components/QuestionPage.jsx
+++ frontend/src/components/QuestionPage.jsx
@@ -11,20 +11,97 @@ import { userLoggedInCheck } from './authHelpers.js';
 export default function QuestionPage(props) {
   const { showErrorAlert, showSuccessAlert } = useAlert();
   const [questionData, setQuestionData] = useState();
+  const [answerData, setAnswerData] = useState();
+  const [comments, setComments] = useState();
+
+  const initialCommentFormState = { 
+    value: '',
+    error: false,
+    helperText: ''
+  }
+  const [commentFormData, setCommentFormData] = useState(initialCommentFormState);
 
   const params = useParams();
   // console.log(params.question_id);
   
+  const fetchComments = async () => {
+    const { data, error } = await supabase.rpc('get_question_comments', { input_question_id: params.question_id });
+    // console.log(data);
+    if (data.length > 0) {
+      setComments(data);
+    }
+  }
+
   useEffect(() => {
     const fetchQuestionData = async () => {
       const { data, error } = await supabase.from('questions').select('*').eq('id', params.question_id);
-      console.log(data);
-      setQuestionData(data[0]);
+      if (data.length > 0) {
+        setQuestionData(data[0]);
+      } else {
+        showErrorAlert('Question does not exist!'); // probably change th
+      }
+      
+    }
+    
+    const fetchAnswerData = async () => {
+      const { data, error } = await supabase.from('question_answers').select('*').eq('question_id', params.question_id);
+      // console.log(data);
+      // setAnswerData(data[0]);
+      if (data.length > 0) {
+        setAnswerData(data[0]);
+      }
     }
 
     fetchQuestionData();
+    fetchAnswerData();
+    fetchComments();
   }, []);
 
+  const handleCommentFormChange = (event) => {
+    setCommentFormData((prevData) => {
+      return {
+        ...prevData,
+        value: event.target.value,
+        error: false,
+        helperText: ''
+      }
+    });
+  }
+
+  const handleClickSubmitComment = async (event) => {
+    event.preventDefault();
+    if (commentFormData.value.trim().length == 0) {
+      setCommentFormData((prevData) => {
+        return {
+          ...prevData,
+          error: true,
+          helperText: `Comment must not be empty`
+        }
+      });
+      return;
+    }
+
+    const { data: sessionData, error: sessionError, loggedIn } = await userLoggedInCheck();
+    if (!loggedIn) {
+      showErrorAlert(sessionError);
+      return;
+    }
+
+    const { data, error: createCommentError } = await supabase.from('question_comments').insert([{
+      question_id: params.question_id,
+      user_id: sessionData.session?.user?.id,
+      comment: commentFormData.value
+    }]);
+
+    if (createCommentError) {
+      showErrorAlert(`${createCommentError.code}: ${createCommentError.message}`);
+      return;
+    }
+
+    setCommentFormData(() => initialCommentFormState);
+    showSuccessAlert('Successfully created new comment');
+    fetchComments(); // fetch comments again after inserting the new comment so that the new comment appears without needing a page refresh 
+  }
 
   return (
     <>
@@ -54,22 +131,36 @@ export default function QuestionPage(props) {
             
           </Box>
           
-          {<Box sx={{ mt: 8, border: '3px solid #1f883d', p: 3}}>
+          {answerData && <Box sx={{ mt: 8, border: '3px solid #1f883d', p: 3}}>
+            <Typography variant="caption">
+              Answered: {new Date(answerData.created_at).toLocaleString('en-US', {
+                dateStyle: 'medium', timeStyle: 'short', timeZone: 'Australia/Sydney'
+              })}
+            </Typography>
             <Typography sx={{ wordBreak: 'break-word', overflowWrap: 'break-word' }}>
-              Placeholder Answer 1. I think you're doing a great job to start off
+              {answerData.answer}
             </Typography>
           </Box>}
           <Box sx={{ display: 'flex', flexDirection: 'column', mt: 5}}>
-            <Typography>
-              Jason: Placeholder comment
-            </Typography>
-            <Typography>
-              Jason: Placeholder comment
-            </Typography>
-            <Typography>
-              Jason: Placeholder comment
-            </Typography>
-            
+            {comments && comments.map(comment => 
+              <Typography sx={{ wordBreak: 'break-word' }}>
+                {new Date(comment.created_at).toLocaleString('en-US', {
+                dateStyle: 'medium', timeStyle: 'short', timeZone: 'Australia/Sydney'
+              })} <b>{comment.username}</b>: {comment.comment}
+              </Typography>)}
+          </Box>
+
+          <Box component="form" sx={{ display: 'flex', alignItems: 'center', gap: 1, mt: 3}}>
+            <TextField 
+              multiline
+              fullWidth
+              label="Add a comment" 
+              value={commentFormData.value}
+              error={commentFormData.error}
+              helperText={commentFormData.helperText}
+              onChange={handleCommentFormChange}
+            ></TextField>
+            <Button type="submit" variant="outlined" onClick={handleClickSubmitComment}>Add comment</Button>
           </Box>
         </Box>
       </Box>}
